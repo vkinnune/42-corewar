d1686e4871e4dfc21d15ba1ec267dfd90ba1f499
Valtteri <valtteri@tekve.fi>
2022-11-01 16:03:24 +0200

    started to do asm parsing different

--- srcs/asm/main.c
+++ srcs/asm/main.c
@@ -43,21 +43,25 @@ void	init_parser()
 	ft_bzero(parse, sizeof(t_parser));
 }
 
-int	label_check(char *line, int len)
+int	label_check(char **p)
 {
 	int	i;
 
 	i = 0;
-	while (i != len)
+	while (1)
 	{
-		if (line[i] == ':')
+		if ((*p)[i] == ':')
+		{
+			*p = &(*p)[i + 1];
 			return (1);
+		}
+		else if ((*p)[i] == '\n' || (*p)[i] == '\0')
+			return (0);
 		i++;
 	}
-	return (0);
 }
 
-void	save_token(char *line, int len, t_token_type token_type)
+void	save_token(char **p, t_token_type token_type)
 {
 	t_token_list	*token_list;
 
@@ -66,11 +70,11 @@ void	save_token(char *line, int len, t_token_type token_type)
 	token_list->token_count++;
 }
 
-int	check_valid(char *line, int len)
+int	check_valid(char **p)
 {
 	t_token_type	token_type;
 
-	if (label_check(line, len))
+	if (label_check(p))
 		token_type = label;
 	/*
 	else if (instruction_check())
@@ -86,36 +90,34 @@ int	check_valid(char *line, int len)
 		*/
 	else
 		return (0);
-	save_token(line, len, token_type);
+	save_token(p, token_type);
 	return (1);
 }
 
-int	move_forward(char **forward_p)
+int	move_forward(char **p)
 {
 	t_source	*source;
 
 	source = get_source();
-	if (**forward_p == '\n')
+	if (**p == '\n')
 	{
 		source->row++;
 		source->col = 0;
 	}
-	else if (**forward_p == '\0')
+	else if (**p == '\0')
 		return (0);
-	(*forward_p)++;
+	(*p)++;
 	source->col++;
 	return (1);
 }
 
-void	handle_asm(char *forward_p, char *stay_p)
+void	handle_asm(char *p)
 {
-	while (move_forward(&forward_p))
+	while (move_forward(&p))
 	{
-		if (*forward_p == ' ' || *forward_p == '\t' || *forward_p == '\n')
+		if (*p == ' ' || *p == '\t' || *p == '\n')
 			continue ;
-		if (check_valid(stay_p, forward_p - stay_p))
-			stay_p = forward_p + 1;
-		else
+		else if (!check_valid(&p))
 		{
 			printf("Error in col: %d row: %d", get_source()->col, get_source()->col);
 			exit(EXIT_FAILURE);
@@ -178,7 +180,7 @@ void	parser(const char *input)
 	//handle header, handle header goes to the start of the asm
 	p = handle_header(input);
 	//handle asm
-	handle_asm(p, p);
+	handle_asm(p);
 }
 
 char *read_file(char *file_name)
