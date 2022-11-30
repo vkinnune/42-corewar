298333fd01430dd9adbc3eb79c9bdc74fae4ee1c
qnguyen <qnguyen@student.hive.fi>
2022-11-30 23:55:15 +0200

    fixed v16

--- srcs/corewar/verbose_sixteen.c
+++ srcs/corewar/verbose_sixteen.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   verbose_sixteen.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jrummuka <jrummuka@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: qnguyen <qnguyen@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/28 15:40:05 by jrummuka          #+#    #+#             */
-/*   Updated: 2022/11/28 15:50:28 by jrummuka         ###   ########.fr       */
+/*   Updated: 2022/11/30 23:51:24 by qnguyen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,10 @@ void	print_sixteen(t_process *process)
 
 	if (process->bytes_to_next > 0)
 	{
-		i = -1;
+		i = 0;
 		ft_printf("ADV %d (0x%0.4x -> 0x%0.4x) ", process->bytes_to_next, process->pc, process->pc + process->bytes_to_next);
-		while (i++ < process->bytes_to_next - 1)
-		{
-			if (g_arena[process->pc + i] < 16)
-			{
-				ft_printf("0%x ", g_arena[process->pc + i]);
-			}
-			else
-				ft_printf("%x ", g_arena[process->pc + i]);
-		}
+		while (i < process->bytes_to_next)
+			ft_printf("%02x ", g_arena[get_position(process->pc + i++)]);
 		ft_printf("\n");
 	}
 }
