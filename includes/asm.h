2c5e84bf557f217fdac68c22b5fc9c128dd02372
Jyri Rummukainen <Jyeri@live.fi>
2022-12-05 20:25:20 +0200

    norme for asm.h

--- includes/asm.h
+++ includes/asm.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   asm.h                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: qnguyen <qnguyen@student.42.fr>            +#+  +:+       +#+        */
+/*   By: jrummuka <jrummuka@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/27 14:37:01 by qnguyen           #+#    #+#             */
-/*   Updated: 2022/12/05 20:17:43 by vkinnune         ###   ########.fr       */
+/*   Updated: 2022/12/05 20:23:48 by jrummuka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,10 +27,10 @@
 # include <stdbool.h>
 # include "op.h"
 
-typedef struct s_label t_label;
-typedef struct s_label_arg t_label_arg;
+typedef struct s_label		t_label;
+typedef struct s_label_arg	t_label_arg;
 
-typedef enum	e_token_type
+typedef enum e_token_type
 {
 	label,
 	instruction,
@@ -42,7 +42,7 @@ typedef enum	e_token_type
 	indirect_label
 }	t_token_type;
 
-typedef enum	e_header_type
+typedef enum e_header_type
 {
 	name,
 	comment
@@ -88,71 +88,70 @@ struct s_label_arg
 	t_label_arg		*next;
 };
 
-typedef	struct s_parser {
+typedef struct s_parser {
 	t_token_list	token_list;
 	t_source		source;
 	t_label			*labels[HASH_SIZE];
 }	t_parser;
 
-void	validate_argument(int ac, char **av);
+void			validate_argument(int ac, char **av);
 
-t_parser	*get_parser(void);
-void	init_parser();
-void	parser(char *input);
-char	*read_file(char *file_name);
-void	print_tokens();
-t_token_list	*get_token_list();
-t_label	**get_labels();
-t_label_arg	**get_label_args(void);
-void	token_check();
-t_source	*get_source();
-t_file	*get_core_file(void);
-int	label_check(char **p);
-int	instruction_check(char **p);
-int	register_check(char **p);
-int	separator_check(char **p);
-int direct_label_check(char **p);
-int	direct_check(char **p);
-char	*handle_header(const char *input);
-void	handle_asm(char *p);
-void	save_token(char **p, char *old_p, t_token_type token_type);
-int	check_instruction(int *i, t_token_list *token_list);
-void	init_name_comment(void);
-char	*check_string_comment_name(bool	*is_newline, char *p);
+t_parser		*get_parser(void);
+void			init_parser(void);
+void			parser(char *input);
+char			*read_file(char *file_name);
+void			print_tokens(void);
+t_token_list	*get_token_list(void);
+t_label			**get_labels(void);
+t_label_arg		**get_label_args(void);
+void			token_check(void);
+t_source		*get_source(void);
+t_file			*get_core_file(void);
+int				label_check(char **p);
+int				instruction_check(char **p);
+int				register_check(char **p);
+int				separator_check(char **p);
+int				direct_label_check(char **p);
+int				direct_check(char **p);
+char			*handle_header(const char *input);
+void			handle_asm(char *p);
+void			save_token(char **p, char *old_p, t_token_type token_type);
+int				check_instruction(int *i, t_token_list *token_list);
+void			init_name_comment(void);
+char			*check_string_comment_name(bool	*is_newline, char *p);
 
 //label.c
 void			handle_label(int *tok_idx);
 unsigned int	hash(char *content);
 t_label			*retrieve_label(char *content);
 void			add_label_list(char *content, t_token_type token_type);
-void			label_list_error();
-void			print_label();
+void			label_list_error(void);
+void			print_label(void);
 
 //label_utils.c
-t_label		*add_label(char *content);
-void		add_l_arg(t_token *toks, int instr_idx, int size);
+t_label			*add_label(char *content);
+void			add_l_arg(t_token *toks, int instr_idx, int size);
 
 //instruction_handler.c
-void	handle_instruction(int *tok_idx);
+void			handle_instruction(int *tok_idx);
 
 //arg_handler.c
-uint8_t	lab_arg(t_token *toks, int instr_idx, int arg_i, int op_size);
-uint8_t	dir_arg(t_token *toks, int arg_i, int op_size);
-uint8_t	ind_arg(t_token *toks, int arg_i);
-uint8_t	reg_arg(t_token *toks, int arg_i);
+uint8_t			lab_arg(t_token *toks, int instr_idx, int arg_i, int op_size);
+uint8_t			dir_arg(t_token *toks, int arg_i, int op_size);
+uint8_t			ind_arg(t_token *toks, int arg_i);
+uint8_t			reg_arg(t_token *toks, int arg_i);
 
 //error.c
-void	check_open_error(int fd);
+void			check_open_error(int fd);
 
 //write.c
-void	write_header(void);
-void	write_intro(uint32_t champ_size);
-void	write_token(void);
-void	write_label(void);
+void			write_header(void);
+void			write_intro(uint32_t champ_size);
+void			write_token(void);
+void			write_label(void);
 
 //utilities.c
-void	cor_init(uint8_t **name, int *fd);
-void	free_label(void);
-void	free_label_arg(void);
+void			cor_init(uint8_t **name, int *fd);
+void			free_label(void);
+void			free_label_arg(void);
 #endif
-
